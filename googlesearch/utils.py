43b0ccebf2669751be54f5141d02a9243fa3344a
Ryan Bagwell <ryan@ryanbagwell.com>
2014-02-13 15:15:31 -0600

    pep8

--- googlesearch/utils.py
+++ googlesearch/utils.py
@@ -3,6 +3,7 @@ from . import *
 
 
 class SearchResults(dict):
+
     """ This is a wrapper that provides easy
         access to common parameters of the
         very complex search results dictionary
@@ -17,12 +18,11 @@ class SearchResults(dict):
     total_results = 0
     search_terms = ''
 
-
     def __init__(self, dict={}):
         super(SearchResults, self).__init__(dict)
 
-        self.total_results = int(self.get('searchInformation', {}).get('totalResults', 0))
+        self.total_results = int(
+            self.get('searchInformation', {}).get('totalResults', 0))
 
-        self.search_terms = self.get('queries', {}).get('request',[])[0].get('searchTerms', '')
-
-
+        self.search_terms = self.get('queries', {}).get(
+            'request', [])[0].get('searchTerms', '')
--- googlesearch/views.py
+++ googlesearch/views.py
@@ -1,27 +1,25 @@
 from django.views.generic import TemplateView
 from apiclient.discovery import build
 from .utils import SearchResults
-from  . import *
-
+from . import *
 
 
 class SearchView(TemplateView):
     template_name = "googlesearch/search_results.html"
-
 
     def get_context_data(self, **kwargs):
 
         context = super(SearchView, self).get_context_data(**kwargs)
 
         service = build("customsearch", GOOGLE_SEARCH_API_VERSION,
-            developerKey=GOOGLE_SEARCH_API_KEY)
+                        developerKey=GOOGLE_SEARCH_API_KEY)
 
         results = service.cse().list(
-                q=self.request.GET.get('q', ''),
-                start=self.page_to_index(),
-                num=GOOGLE_SEARCH_RESULTS_PER_PAGE,
-                cx=GOOGLE_SEARCH_ENGINE_ID,
-            ).execute()
+            q=self.request.GET.get('q', ''),
+            start=self.page_to_index(),
+            num=GOOGLE_SEARCH_RESULTS_PER_PAGE,
+            cx=GOOGLE_SEARCH_ENGINE_ID,
+        ).execute()
 
         results = SearchResults(results)
 
@@ -42,17 +40,15 @@ class SearchView(TemplateView):
         pages = self.calculate_pages()
 
         context.update({
-                'items': results.items,
-                'total_results': results.total_results,
-                'current_page': pages[1],
-                'prev_page': pages[0],
-                'next_page': pages[2],
-                'search_terms': results.search_terms,
-            })
-
+            'items': results.items,
+            'total_results': results.total_results,
+            'current_page': pages[1],
+            'prev_page': pages[0],
+            'next_page': pages[2],
+            'search_terms': results.search_terms,
+        })
 
         return context
-
 
     def calculate_pages(self):
         """ Returns a tuple consisting of
@@ -62,14 +58,10 @@ class SearchView(TemplateView):
         current_page = int(self.request.GET.get('p', 1))
         return (current_page - 1, current_page, current_page + 1,)
 
-
-
     def page_to_index(self, page=None):
-
         """ Converts a page to the start index """
 
         if page is None:
             page = self.request.GET.get('p', 1)
 
-        return int(page) * int(GOOGLE_SEARCH_RESULTS_PER_PAGE) + 1 -  int(GOOGLE_SEARCH_RESULTS_PER_PAGE)
-
+        return int(page) * int(GOOGLE_SEARCH_RESULTS_PER_PAGE) + 1 - int(GOOGLE_SEARCH_RESULTS_PER_PAGE)
