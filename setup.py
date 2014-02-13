ed81fe5d915e7ac8f3ec5c8ea68115b4a556f807
Ryan Bagwell <ryan@ryanbagwell.com>
2014-02-13 15:41:14 -0600

    still trying to add templates dir to package

--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='django-google-search',
     version='1.0',
     author='Ryan Bagwell',
     author_email='ryan@ryanbagwell.com',
-    packages=['googlesearch'],
+    packages=find_packages(),
+    package_data={
+        'googlesearch': ['templates/*.*', 'templatetags/*.*'],
+    },
+    include_package_data=True,
     url='https://github.com/hzdg/django-google-search',
     license='license.txt',
     description='A interface to use Google Custom Search in Django templates',
     long_description=open('README.rst').read(),
-    include_package_data=True,
     install_requires=[
         "Django >= 1.3",
         "requests >= 2.0",
