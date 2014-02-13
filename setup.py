b6eb2ca50e6b1db7b95a4b33ec7b46a556c4bbc2
Ryan Bagwell <ryan@ryanbagwell.com>
2014-02-13 10:57:41 -0600

    changing requests version, correcting format of name, bumping version

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from distutils.core import setup
 
 setup(
-    name='Django Google Search',
-    version='0.1.0',
+    name='django-google-search',
+    version='0.1.1',
     author='Ryan Bagwell',
     author_email='ryan@ryanbagwell.com',
     packages=['googlesearch'],
-    url='https://github.com/ryanbagwell/django-cms-nivo-slider',
+    url='https://github.com/hzdg/django-google-search',
     license='license.txt',
     description='A interface to use Google Custom Search in Django templates',
     long_description=open('README.rst').read(),
     include_package_data=True,
     install_requires=[
         "Django >= 1.3",
-        "requests >= 1.1.0",
+        "requests >= 2.0",
     ],
 )
