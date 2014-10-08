d3f50957040bb83c952e874d2f3e669d718d01e2
Colin Wood <cwood06@gmail.com>
2014-10-08 10:37:02 -0400

    Bump minior version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-google-search',
-    version='1.0',
+    version='1.1',
     author='Ryan Bagwell',
     author_email='ryan@ryanbagwell.com',
     packages=find_packages(),
